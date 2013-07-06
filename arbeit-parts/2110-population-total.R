7c01cc321094ae09c6a66a40b921c9bc201e6bd1
Felix Rabe <public@felixrabe.net>
2013-07-06 19:09:16 +0200

    Work

--- arbeit-parts/2110-population-total.R
+++ arbeit-parts/2110-population-total.R
@@ -1,1 +1,7 @@
 ppsum <- ddply(p[p$area == 'WORLD', c('year', 'population')], .(year), colwise(sum))
+
+ppsum_linear <- data.frame(year = ppsum$year, population = seq(2525778669, 6916183482, length.out = 61))
+
+ppsum <- rbind(data.frame(ppsum, type='raw'), data.frame(ppsum_linear, type='linear'))
+
+# ppsum <- merge(ppsum, ppsum_linear)
