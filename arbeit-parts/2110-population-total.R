d65ffeba26a2fad0a48a923aa94eb0c299ebea78
Felix Rabe <public@felixrabe.net>
2013-07-07 01:13:06 +0200

    Work

--- arbeit-parts/2110-population-total.R
+++ arbeit-parts/2110-population-total.R
@@ -1,3 +1,5 @@
 population_total <- ddply(p[p$area == 'WORLD', c('year', 'population')], .(year), colwise(sum))
 
 population_total_plot <- ggplot(population_total, aes(year, population)) + geom_line() + scale_y_continuous(label = f, lim = c(0, 8000000000)) + labs(y = 'Weltbevölkerung', x = 'Jahr')
+
+population_total_table <- head(population_total)
