4eaeb6cfb09aca373a5f4e1d45d6a784cad65a2c
Felix Rabe <public@felixrabe.net>
2013-07-06 23:13:34 +0200

    Work

--- arbeit-parts/2110-population-total.R
+++ arbeit-parts/2110-population-total.R
@@ -1,3 +1,3 @@
 population_total <- ddply(p[p$area == 'WORLD', c('year', 'population')], .(year), colwise(sum))
 
-population_total_plot <- ggplot(population_total, aes(year, population)) + geom_line() + geom_smooth(method = 'lm', se = FALSE) + scale_y_continuous(label = f, lim = c(0, 8000000000)) + labs(y = 'Weltbevölkerung', x = 'Jahr')
+population_total_plot <- ggplot(population_total, aes(year, population)) + geom_line() + scale_y_continuous(label = f, lim = c(0, 8000000000)) + labs(y = 'Weltbevölkerung', x = 'Jahr')
--- arbeit-parts/2120-population-by-age.R
+++ arbeit-parts/2120-population-by-age.R
@@ -1,11 +1,12 @@
-# pp <- ddply(p[p$area == 'WORLD', c('year', 'age', 'population')], c('year', 'age'), colwise(sum))
-
-pp <- ddply(p[p$area == 'WORLD', c('year', 'age', 'sex', 'population')], .(year, age), summarize, sex = sex, population = population, ratio = population / sum(population))
-
-# ppmale <- pp[pp$sex == 'male',]
-
-# qplot(year, age, data=ppmale, alpha = I(1/2), color = ratio, size = population) + scale_y_discrete(limits = ages) + scale_size_continuous(label = f) + labs(x = 'Jahr', y = 'Altersgruppe', size = 'Bevölkerung', color = 'Anteil männlich') + scale_color_continuous(label = percent)
+pp <- p[p$area == 'WORLD', c('year', 'age', 'sex', 'population')]
 
 smaller_guide <- guide_legend(keywidth = 0.8, keyheight = 0.8)
 
-population_by_age_plot <- ggplot(pp, aes(year, color = age)) + geom_line(aes(y = population, linetype = sex)) + scale_color_discrete(limits = ages) + scale_y_continuous(label = f) + labs(x = 'Jahr', y = 'Bevölkerung in der Gruppe', linetype = 'Geschlecht', color = 'Altersgruppe') + guides(linetype = smaller_guide, color = smaller_guide) + scale_linetype_discrete(labels = c('weiblich', 'männlich'))
+plt <- ggplot(pp, aes(year, color = age))
+plt <- plt + geom_line(aes(y = population, linetype = sex))
+plt <- plt + scale_color_discrete(limits = ages)
+plt <- plt + scale_y_continuous(label = f)
+plt <- plt + labs(x = 'Jahr', y = 'Bevölkerung in der Gruppe', linetype = 'Geschlecht', color = 'Altersgruppe')
+plt <- plt + guides(linetype = smaller_guide, color = smaller_guide)
+plt <- plt + scale_linetype_discrete(labels = c('weiblich', 'männlich'))
+plt -> population_by_age_plot
