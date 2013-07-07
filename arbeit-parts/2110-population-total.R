9b3a1133d093c8ebbef1f776e6298ff6f62cbbf0
Felix Rabe <public@felixrabe.net>
2013-07-07 13:17:06 +0200

    Work

--- arbeit-parts/2110-population-total.R
+++ arbeit-parts/2110-population-total.R
@@ -3,3 +3,7 @@ population_total <- ddply(p[p$area == 'WORLD', c('year', 'population')], .(year)
 population_total_plot <- ggplot(population_total, aes(year, population)) + geom_line() + scale_y_continuous(label = f, lim = c(0, 8000000000)) + labs(y = 'Weltbevölkerung', x = 'Jahr')
 
 population_total_table <- population_total[population_total$year >= 1970 & population_total$year < 2010,]
+
+population_total_table$population <- f(population_total_table$population)
+
+population_total_xtable <- xtable(population_total_table, caption='Tabelle der ausgewerteten Beobachtungen, 1970 - 2009', label='population_total_table')
