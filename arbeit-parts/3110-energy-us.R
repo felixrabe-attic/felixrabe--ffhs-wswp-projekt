59f098b6a04a4e10b62af3e00cd98a56ba18a190
Felix Rabe <public@felixrabe.net>
2013-07-07 22:58:24 +0200

    wip

--- arbeit-parts/3110-energy-us.R
+++ arbeit-parts/3110-energy-us.R
@@ -3,7 +3,7 @@ e_us <- e[e$area == 'United States of America',]
 p_us <- ddply(p[p$area == 'United States of America', c('year', 'population')], .(year), colwise(sum))
 
 subs <- function(df) {
-  subset(df, c(year, type, value))
+  subset(df, select = c(year, type, value))
 }
 
 pc_us <- rbind(
@@ -12,9 +12,7 @@ pc_us <- rbind(
   subs(transform(merge(e_us, p_us), type = 'toepc', value = consumption * 1000000 / population))
 )
 
-pp <- ggplot(pc_us, aes(year))
-pp <- pp + geom_line(aes(y = consumption))
-pp <- pp + geom_line(aes(y = per_capita))
+pp <- ggplot(pc_us, aes(year, value, color = type))
 pp <- pp + scale_y_continuous(lim = c(0, 4000))
 pp <- pp + labs(x = 'Jahr', y = 'Verbrauch in Mio. t Öläquivalenten')
 pp -> pc_us_plot
