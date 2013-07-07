8bfb06a18140ef9cd199aee837021ee210d2e10a
Felix Rabe <public@felixrabe.net>
2013-07-08 01:46:19 +0200

    Really show top 10

--- arbeit-parts/4110-names-top.R
+++ arbeit-parts/4110-names-top.R
@@ -1,5 +1,6 @@
 # name,rank,count,prop100k,cum_prop100k,pctwhite,pctblack,pctapi,pctaian,pct2prace,pcthispanic
 
 ntab_display <- transform(subset(usnames, select = c(name, rank, count)), count = f(count))
+ntab_display <- head(ntab_display, n = 10)
 names(ntab_display) <- c('Name', 'Rang', 'Anzahl')
-names_top_table <- xtable(head(ntab_display), caption='Top 10 US-Namen im Jahr 2000.', label='names_top_table')
+names_top_table <- xtable(ntab_display, caption='Top 10 US-Namen im Jahr 2000.', label='names_top_table')
