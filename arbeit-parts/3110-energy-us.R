890a5dacc0642ce3423f1db52ea5797cd058f895
Felix Rabe <public@felixrabe.net>
2013-07-07 23:37:47 +0200

    wip

--- arbeit-parts/3110-energy-us.R
+++ arbeit-parts/3110-energy-us.R
@@ -12,9 +12,16 @@ pc_us <- rbind(
   subs(transform(merge(e_us, p_us), type = 'toepc', value = consumption * 1000000 / population))
 )
 
-pp <- ggplot(pc_us, aes(year, value, color = type))
+types = levels(pc_us$type)
+
+levels(pc_us$type)[which(types == 'mtoe')]  <- 'Gesamtverbrauch in MTOE'
+levels(pc_us$type)[which(types == 'pop')]   <- 'Bevölkerung'
+levels(pc_us$type)[which(types == 'toepc')] <- 'Pro-Kopf-Verbrauch in TOE'
+
+
+pp <- ggplot(pc_us, aes(year, value))
 pp <- pp + geom_line()
-# pp <- pp + scale_y_continuous(lim = c(0, 4000))
 pp <- pp + labs(x = 'Jahr', y = 'Wert')
-pp <- pp + facet_grid(type ~ .)
+pp <- pp + facet_grid(type ~ ., scales = 'free')
+pp <- pp + scale_y_continuous(label = f)
 pp -> pc_us_plot
