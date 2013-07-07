7cc40f14155e3143f0d7234efe2ded4dec593f56
Felix Rabe <public@felixrabe.net>
2013-07-08 01:43:12 +0200

    Fix (S)

--- arbeit-parts/0260-setup-load-names-data.R
+++ arbeit-parts/0260-setup-load-names-data.R
@@ -1,2 +1,2 @@
 usnames <- read.csv('../names/020-csv/names.csv',
-  colClasses = c('character', 'integer', 'integer', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+  colClasses = c('character', 'integer', 'integer', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'), na.strings = c('(S)'))
