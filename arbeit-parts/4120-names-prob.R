66c18ce8d074af9524dafbf3a3c18e25a8172f50
Felix Rabe <public@felixrabe.net>
2013-07-08 11:27:23 +0200

    Work

--- arbeit-parts/4120-names-prob.R
+++ arbeit-parts/4120-names-prob.R
@@ -1,5 +1,5 @@
 prob_omega <- sum(p[p$area == 'WORLD' & p$year == 2000, c('population')])
 
-prob_a <- ntab[ntab$name == 'SMITH', c('count')]
+prob_a_cap_b <- ntab[ntab$name == 'SMITH', c('count')]
 
 prob_b <- sum(p[p$area == 'United States of America' & p$year == 2000, c('population')])
